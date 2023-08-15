c5370c68656303e34121b87628cbeef2c174dab6
stuartore <stuartore@foxmail.com>
2023-08-15 12:39:17 +0800

    psyche: minor changes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,11 +3,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/twrp_alioth.mk \
-	$(LOCAL_DIR)/twrp_munch.mk \
 	$(LOCAL_DIR)/twrp_psyche.mk
 
 COMMON_LUNCH_CHOICES := \
-	twrp_alioth-eng \
-	twrp_munch-eng \
-	twrp_psyche-eng
+	twrp_psyche-userdebug
